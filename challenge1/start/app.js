f5b6448317e12963879f0d56db86f99367918600
Aravindh Balakrishnan <abalakrishnan0@walmartlabs.com>
2015-12-30 08:58:03 -0800

    complete challenge1

--- challenge1/start/app.js
+++ challenge1/start/app.js
@@ -3,9 +3,8 @@ var http = require('http'),
     fs = require('fs');
 
 http.createServer(function(request, response) {
-
-    // TODO: Find the ASYNCHRONOUS, NON-BLOCKING API for reading in a file.
-    fs.CHANGE_ME('./data.csv', 'utf-8', function(err, data) {
+    
+    fs.readFile('./data.csv', 'utf-8', function(err, data) {
         var responseData = {};
 
         // Basic JS: Work with the data in the file, and create the response
@@ -17,13 +16,11 @@ http.createServer(function(request, response) {
             responseData[parts[0]] = parts[1];
         });
 
-        // TODO: How do we set the content type we're sending back?
         response.writeHead(200, {
-            'CHANGEME':'CHANGEME'
+            'Content-type':'application/json'
         });
-
-        // TODO: How do we serialize responseData to a JSON string?
-        response.end(CHANGE_ME.CHANGE_ME(responseData));
+        
+        response.end(JSON.stringify(responseData));
 
     });
 
